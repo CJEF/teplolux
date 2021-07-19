7b3a8538b2452a31a9ccae38ef1548a60eb70195
CJEF <ef2200@mail.ru>
2021-07-19 17:25:36 +0600

    full desktop html layout

--- src/js/main.js
+++ src/js/main.js
@@ -5,23 +5,52 @@
   * @returns {number}
   */
 
- const swiper = new Swiper('.article-slider', {
-  // Optional parameters
-  loop: true,
-
-  // If we need pagination
-  pagination: {
+ const swiper = new Swiper(".swiper-container", {
+   direction: "horizontal",
+   // loop: true,
+   // If we need pagination
+   pagination: {
     el: '.swiper-pagination',
-  },
+    clickable: true,
+   },
+   // Navigation arrows
+   // navigation: {
+   //   nextEl: '.swiper-button-next',
+   //   prevEl: '.swiper-button-prev',
+   // },
+   // And if we need scrollbar
+   // scrollbar: {
+   //   el: '.swiper-scrollbar',
+   // },
+ });
 
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  if (anchor) {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
-});
+      const blockID = anchor.getAttribute("href").substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+}
+
+/* custom slider */
+const detailBtn = document.querySelectorAll(".detail-btn-js");
+detailBtn.forEach(function(btn) {
+  btn.addEventListener("click", function (e) {
+    const article = this.closest(".catalog-article");
+    const wrapper = article.querySelector(".article-info__wrapper");
+    wrapper.classList.toggle("active");
+    if (!wrapper.classList.contains('active')) {
+      this.textContent = "← Детальная информация";
+    } else {
+      this.textContent = "← Назад";
+    }
+  });
+})
